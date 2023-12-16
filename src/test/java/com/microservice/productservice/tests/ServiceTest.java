78aa6b64c5ea5c646f5852c45a091fd31ad98516
Chakir-Aicha <aicha.chaki.2022@gmail.com>
2023-12-16 11:43:27 +0100

    commit  2

---
+++ src/test/java/com/microservice/productservice/tests/ServiceTest.java
@@ -0,0 +1,40 @@
+package com.microservice.productservice.tests;
+
+import com.microservice.productservice.domain.model.Product;
+import com.microservice.productservice.dto.ProductResponse;
+import com.microservice.productservice.repository.ProductRepository;
+import com.microservice.productservice.service.ProductService;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
+@ExtendWith(MockitoExtension.class)
+public class ServiceTest {
+        @Mock
+        private ProductRepository productRepository;
+
+        @InjectMocks
+        private ProductService productService;
+
+        @Test
+        public void shouldReturnAllProducts() {
+            // given
+            List<Product> productList = new ArrayList<>();
+
+            // when
+            when(productRepository.findAll()).thenReturn(productList);
+
+            // action
+            List<ProductResponse> result = productService.getAllProducts(); // assuming productService calls productRepository.findAll()
+
+            // assert
+            assertEquals(productList.size(), result.size());
+        }
+}
